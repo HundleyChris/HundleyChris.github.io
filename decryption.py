b9f2ea9e01ce384303fb2f582886c93ea7a3f84f
hundleyChris <christopherhun.71763@redlandsschools.net>
2018-12-12 05:28:31 +0000

    Final

---
+++ decryption.py
@@ -0,0 +1,11 @@
+statement = "Rw3Ujwj%nx%httq&"
+for ch in statement:
+    x = ord(ch)
+    print(x)
+    x = x - 5
+    print(x)
+    y = chr(x)
+    print(y)
+    decrypt = decrypt+ y
+    print(decrypt)
+
